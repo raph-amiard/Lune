19c45fa7414cfdb020be7a08dbe5af6c22bcd3f6
Raphaël AMIARD <raph.amiard@gmail.com>
2012-10-16 13:48:01 +0100

    fix

--- src/main/scala/VarMap.scala
+++ src/main/scala/VarMap.scala
@@ -5,7 +5,7 @@ class VarMap(varmap : Map[String, AbstractType]) {
   def getType(binding : String) = varmap(binding).get()
 
   def bindName(binding: String) : VarMap =
-    new VarMap(vmap + ((binding, new TypePoly)))
+    new VarMap(varmap + ((binding, new TypePoly)))
 
   def bindNames(bindings : List[String]) : VarMap = 
     new VarMap(bindings.foldLeft(varmap)((vmap, binding) => vmap + ((binding, new TypePoly()))))
